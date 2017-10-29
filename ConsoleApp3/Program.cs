f5d93e381a9b987e0468fb8da17508da90421292
Paul Slavnov <pavel.shakhov.92@gmail.com>
2017-10-29 18:33:59 +0300

    new commit at 29.10

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace TwoDimensionalMassiveByRandom
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите m: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Заполнение массива:");
+            int[,] mas = new int[n, m];
+
+            Random r = new Random(); // объявление переменной для генерации случайных чисел
+
+            for (int i = 0; i < n; ++i) // массив заполняется случайными числами 
+            {       for (int j = 0; j < m; j++)
+                {            mas[i, j] = r.Next(10); // от 0 до 9       
+                }
+            }
+
+            Console.WriteLine("Полученный массив:");
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
         }
     }
 }
